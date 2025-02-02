8093e8006da342547d0b82bca7f90864640f0487
PavelZanek <zanek.pavel@gmail.com>
2025-02-02 02:50:03 +0100

    chore: conflicts after merge

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,9 +15,10 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
+    use HasApiTokens;
+
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
-    use HasApiTokens;
     use HasProfilePhoto;
     use HasTeams;
     use Notifiable;
